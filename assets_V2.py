861f10afa666bcb6bd2d872268ec509f16c1228f
Ron Bruins <ron@brunij.nl>
2026-04-16 11:03:17 +0200

    Refactored tasks

--- assets_V2.py
+++ assets_V2.py
@@ -5,8 +5,8 @@ import tasks
 os.system('clear')
 
 # task="createloop"
-task="updateloop"
-# task="libloop"
+# task="updateloop"
+task="libloop"
 # task="tagloop"
 # task="tagdelete"
 # task="deleteloop"
@@ -26,25 +26,12 @@ init_users['9'] = "Greetje Nijburg"
 # user_exec=["4"]
 user_exec=["2","3","4","5","9","1"]
 # album_dict = {}
-task == "debug"
+# task == "debug"
+
 def main():
-    if task == "createloop":
-        tasks.createloop(user_exec)
-        tasks.update_album_list(user_exec)
-    elif task == "deleteloop":
-        tasks.deleteloop(user_exec)
-    elif task == "tagloop":                         # NEW
-         tasks.mainexecutor(user_exec,task)         # NEW
-    elif task == "libloop":                         # NEW
-         tasks.mainexecutor(user_exec,task)         # NEW
-    elif task == "updateloop":                      # NEW
-        tasks.mainexecutor(user_exec,task)          # NEW
-    elif task == "tagdelete":
-        tasks.tagdelete(user_exec)
-
-
-# def warn(init_user):
-#     wait = input(f"Task: {task.upper()} \nUser: {init_user} \n\n#####      Are you sure, then press ENTER  #####\n#####        Or CTRL-C to stop             #####")
+    tasks.mainexecutor(user_exec,task) 
+    if task == "createloop": 
+        tasks.mainexecutor(user_exec,"updateloop")
 
 if __name__ == '__main__':
     main()
--- tasks.py
+++ tasks.py
@@ -12,125 +12,88 @@ admin_api = settings.admin_api
 assetid=settings.assetid
 init_users = settings.init_users
 asset_limit = settings.asset_limit
-# YELLOW =  settings.YELLOW
-# RESET = settings.RESET
+
 
 update_album_dict = {}
+album_list = {}
+album_final = dict(album_list)
 
 def mainexecutor(user_exec, task):
+    album_dict={}
     for iun in user_exec:
         init_user = init_users[iun]
         api_key = api_keys[init_user]
         rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
         if task == "libloop":
-            libloop(user_exec,rbimmich)
+            libloop(rbimmich)
         elif task == "tagloop":
-            tagloop(user_exec,rbimmich)
+            tagloop(rbimmich)
+        elif task == "tagdelete":
+            tagdelete(rbimmich)
+        elif task == "deleteloop":
+            deleteloop(rbimmich)
         elif task == "updateloop":
-            update_album_dict = update_album_list(user_exec,rbimmich,api_key)
-            print(update_album_dict)
+            update_album_dict = update_album_list(rbimmich,api_key)
+        elif task == "createloop":
+            createloop(rbimmich,init_user,api_key,album_dict)
+
     if task == "updateloop":
         sorted_data_keys = json.dumps({k: update_album_dict[k] for k in sorted(update_album_dict)})
         update_album_dict = json.loads(sorted_data_keys)
-        update_albums = rbimmich.update_albums(update_album_dict)
+        rbimmich.update_albums(update_album_dict)
 
+    elif task == "createloop":
+        sorted_data_keys = json.dumps({k: album_dict[k] for k in sorted(album_dict)})
+        album_dict = json.loads(sorted_data_keys)
+        for k,v in album_dict.items():
+            print(k)
+        rbimmich.createAlbum(album_dict,album_final) 
 
-def libloop(user_exec,rbimmich):
-    # for iun in user_exec:
-    #     init_user = init_users[iun]
-    #     api_key = api_keys[init_user]
-    #     rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
+def libloop(rbimmich):
         libraries = rbimmich.get_libraries() 
         print(json.dumps(libraries))
 
-def tagloop(user_exec,rbimmich):
-    # for iun in user_exec:
-    #     #  for iun in "1":
-    #     init_user = init_users[iun]
-    #     api_key = api_keys[init_user]
-    #     rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
+def tagloop(rbimmich):
         taglist = rbimmich.gettags()
-        print(taglist)
-        # for a in taglist:
-        #     print(a['id'])   
         return taglist
 
-def tagdelete(user_exec):
-    for iun in user_exec:
-        init_user = init_users[iun]
-        api_key = api_keys[init_user]
-        rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
-        taglist = rbimmich.gettags()
-        for tag in taglist:
-            tag_id=tag['id']
-            print(f"deleting {tag_id}")
-            rbimmich.delete_tags(tag_id)
-
-
-
-def deleteloop(user_exec):
-    for iun in user_exec:
-        init_user = init_users[iun]
-        api_key = api_keys[init_user]
-        rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
-        local=False
-        album_ids,album_list = rbimmich.get_albums(local)
-        for album_id in album_ids:
-            print(f"deleting {album_id}")
-            rbimmich.delete_album(album_id)
-
-def update_album_list(user_exec,rbimmich,api_key):
-    # update_album_dict = {}
-    # for iun in user_exec:
-    #     init_user = init_users[iun]
-    #     api_key = api_keys[init_user]
-    #     rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
+def update_album_list(rbimmich,api_key):
     local=True
     album_ids,album_list = rbimmich.get_albums(local)
-
     for albumname in album_list['album']:
         album_id = album_list['album'][albumname]
         update_album_dict[albumname]={}
         update_album_dict[albumname]['album_id']=album_id
         update_album_dict[albumname]['api_key']=api_key
     return update_album_dict
-            # print(albumname, api_key)
-    # sorted_data_keys = json.dumps({k: update_album_dict[k] for k in sorted(update_album_dict)})
-    # update_album_dict = json.loads(sorted_data_keys)
-    
-    # update_albums = rbimmich.update_albums(update_album_dict)
 
-def createloop(user_exec):
-    album_dict={}
-    album_list = {}
-    album_final = dict(album_list)
-    # album_list = {}
-    for iun in user_exec:
-        init_user = init_users[iun]
-        api_key = api_keys[init_user]
-        rbimmich = imclass_V2.ImmichApi(api_key,base_url,init_user,admin_api)
-        local = False
+def tagdelete(rbimmich):
+    taglist = rbimmich.gettags()
+    for tag in taglist:
+        tag_id=tag['id']
+        print(f"deleting {tag_id}")
+        rbimmich.delete_tags(tag_id)
+
+def deleteloop(rbimmich):
+        local=False
         album_ids,album_list = rbimmich.get_albums(local)
-        # create_albums(rbimmich,album_dict,init_user,api_key)
-        # assetsReceived,AlbumUsers = init_album_build(rbimmich,init_user)
-
-        immich_users = rbimmich.get_users()
-        AlbumUsers,set_user_id = rbimmich.build_album_users(immich_users,init_user,to_share)
-        print(f"User ID: \t \t \t \t {set_user_id}")
-        libraries = rbimmich.get_libraries() 
-        search_lib = rbimmich.get_search_lib(set_user_id,libraries)
-        assetsReceived = rbimmich.get_assets(asset_limit,search_lib)
+        for album_id in album_ids:
+            print(f"deleting {album_id}")
+            rbimmich.delete_album(album_id)
 
 
-        print("Initializing Folders build")
-        user_album_dict = rbimmich.build_album_dict(assetsReceived,AlbumUsers,init_user,album_dict,api_key)
-        print("Initializing Tags build")
-        tag_album_dict = rbimmich.build_album_dict_by_tag(assetsReceived,AlbumUsers,init_user,user_album_dict,api_key)
-        album_dict = tag_album_dict
-        album_final.update(album_list['album'])
-    
-    sorted_data_keys = json.dumps({k: album_dict[k] for k in sorted(album_dict)})
-    album_dict = json.loads(sorted_data_keys)
-    for k,v in album_dict.items():
-        print(k)
-    rbimmich.createAlbum(album_dict,album_final) 
+def createloop(rbimmich,init_user,api_key,album_dict):
+    local = False
+    album_ids,album_list = rbimmich.get_albums(local)
+    immich_users = rbimmich.get_users()
+    AlbumUsers,set_user_id = rbimmich.build_album_users(immich_users,init_user,to_share)
+    print(f"User ID: \t \t \t \t {set_user_id}")
+    libraries = rbimmich.get_libraries() 
+    search_lib = rbimmich.get_search_lib(set_user_id,libraries)
+    assetsReceived = rbimmich.get_assets(asset_limit,search_lib)
+    print("Initializing Folders build")
+    user_album_dict = rbimmich.build_album_dict(assetsReceived,AlbumUsers,init_user,album_dict,api_key)
+    print("Initializing Tags build")
+    tag_album_dict = rbimmich.build_album_dict_by_tag(assetsReceived,AlbumUsers,init_user,user_album_dict,api_key)
+    album_dict = tag_album_dict
+    album_final.update(album_list['album'])
